41b60ecba106ca7984caeae6b54511b6adede066
NAMEIKE <2298072707@qq.com>
2020-02-10 17:46:23 +0800

    注册模块-手机验证和头像上传

--- src/views/login/components/register.vue
+++ src/views/login/components/register.vue
@@ -6,7 +6,21 @@
     center
     :visible.sync="dialogFormVisible"
   >
-    <el-form status-icon  :model="form" :rules="rules" ref="registerForm">
+    <el-form status-icon :model="form" :rules="rules" ref="registerForm">
+      <el-form-item label="头像">
+        <!-- 用户头像 -->
+        <el-upload
+          class="avatar-uploader"
+          :action="upUrl"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          name="image"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
       <el-form-item label="昵称" prop="username" :label-width="formLabelWidth">
         <el-input v-model="form.username" autocomplete="off"></el-input>
       </el-form-item>
@@ -26,7 +40,7 @@
       <el-form-item label="验证码" :label-width="formLabelWidth">
         <el-row>
           <el-col :span="16">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
+            <el-input v-model="form.code" autocomplete="off"></el-input>
           </el-col>
           <el-col :span="7" :offset="1" class="register-box">
             <img
@@ -44,7 +58,9 @@
             <el-input v-model="form.name" autocomplete="off"></el-input>
           </el-col>
           <el-col :span="7" :offset="1">
-            <el-button>点击获取验证码</el-button>
+            <el-button @click="getSMS" :disabled="delay != 0">
+              {{ delay == 0 ? "点击获取验证码" : `还有${delay}秒可以获取` }}
+            </el-button>
           </el-col>
         </el-row>
       </el-form-item>
@@ -59,6 +75,7 @@
 </template>
 
 <script>
+import axios from "axios";
 // 验证手机号的 函数
 const checkPhone = (rule, value, callback) => {
   // 接收参数 value
@@ -96,14 +113,22 @@ export default {
   data() {
     return {
       dialogFormVisible: false,
-      codeURL: process.env.VUE_APP_URL+'/captcha?type=sendsms',
+      codeURL: process.env.VUE_APP_URL + "/captcha?type=sendsms",
+      // 倒计时时间
+      delay: 0,
+      // 本地图片预览地址
+      imageUrl: "",
+      // 头像上传的接口地址
+      upUrl: process.env.VUE_APP_URL + "/uploads",
       form: {
         username: "",
         password: "",
         // 手机号
         phone: "",
         // 邮箱
-        email: ""
+        email: "",
+        code: "",
+        name: ""
       },
       rules: {
         username: [
@@ -134,15 +159,93 @@ export default {
   },
   methods: {
     // 点击改变验证码
-    changeCode(){
-      this.codeURL = process.env.VUE_APP_URL+'/captcha?type=sendsms&t='+Date.now();
+    changeCode() {
+      this.codeURL =
+        process.env.VUE_APP_URL + "/captcha?type=sendsms&t=" + Date.now();
+    },
+    // 获取短信验证码
+    getSMS() {
+      if (this.delay == 0) {
+        this.delay = 60;
+        const timeID = setInterval(() => {
+          this.delay--;
+          if (this.delay == 0) {
+            clearInterval(timeID);
+          }
+        }, 1000);
+      }
+      // 调用接口
+      axios({
+        url: process.env.VUE_APP_URL + "/sendsms",
+        method: "post",
+        data: {
+          code: this.form.code,
+          phone: this.form.phone
+        },
+        withCredentials: true
+      }).then(res => {
+        //成功回调
+        // window.console.log(res)
+        if (res.data.code == 200) {
+          this.$message.success("验证码获取成功" + res.data.data.captcha);
+        } else if (res.data.code == 0) {
+          this.$message.error(res.data.message);
+        }
+      });
+    },
+    // 上传头像
+    handleAvatarSuccess(res, file) {
+      window.console.log(res)
+      // URL.createObjectURL 生成的是本地的临时路径,刷新就没用了
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    // 上传之前的效验函数
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg" || "imgage/png";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
     }
-  },
+  }
 };
 </script>
 
 <style lang="less">
 .register {
+  // 头像代码
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader {
+    text-align: center;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+  //上面是头像
   .el-dialog__header {
     background: linear-gradient(to right, #01c5fb, #1197f9);
   }
