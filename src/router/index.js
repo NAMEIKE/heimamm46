607e4f5ca37a6622f17089e5f06cce2de50b80e9
NAMEIKE <2298072707@qq.com>
2020-02-12 02:18:40 +0800

    首页模块 - 简单布局

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,17 @@ import VueRouter from 'vue-router'
 // 注册路由
 Vue.use(VueRouter)
 import login from '../views/login/login.vue'
+import index from '../views/index/index.vue';
 // 路由实例化
 const router = new VueRouter({
   routes: [
     {
       path: '/login',
       component: login
+    },
+    {
+      path: '/index',
+      component: index
     },
   ]
 });
---
+++ src/views/index/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-container class="my-container">
+    <el-header class="my-header">
+      <div class="left">
+        <i class="el-icon-s-fold"></i>
+        <img src="../../assets/矢量智能对象 拷贝 9.png" alt="" />
+        <span>黑马面面</span>
+      </div>
+      <div class="right">
+        <img :src="userIcon" alt="" />
+        <span class="name">{{ username }},您好</span>
+        <el-button type="primary">退出</el-button>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="my-aside">Aside</el-aside>
+      <el-main class="my-main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.my-container {
+  height: 100%;
+  .my-header {
+    display: flex;
+    // 左右均分
+    justify-content: space-between;
+    // 上下居中
+    align-items: center;
+    height: 60px;
+  }
+  .left {
+    display: flex;
+    align-items: center;
+    i {
+      font-size: 24px;
+      height: 20px;
+      margin-right: 22px;
+    }
+    img {
+      margin-right: 11px;
+      width: 33px;
+      height: 28px;
+    }
+    span {
+      font-size: 22px;
+      font-family: Microsoft YaHei;
+      font-weight: bold;
+      color: #49A1FF;
+    }
+  }
+  .right {
+    display: flex;
+    align-items: center;
+    img {
+      width: 43px;
+      height: 43px;
+      border-radius: 50%;
+      margin-right: 9px;
+    }
+    span.name {
+      margin-right: 38px;
+      font-size: 14px;
+    }
+  }
+  .my-aside {
+    background: yellowgreen;
+  }
+  .my-main {
+    background: #0094ff;
+  }
+}
+</style>
