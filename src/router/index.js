eca22ce18d56a0608d4c4acc20e36a81d486d066
NAMEIKE <2298072707@qq.com>
2020-02-12 14:25:30 +0800

    首页模块 - 嵌套路由整合

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,13 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 import login from '../views/login/login.vue'
 import index from '../views/index/index.vue';
+// 导入嵌套的组件
+import chart from '../views/index/chart/chart.vue'
+import enterprise from '../views/index/enterprise/enterprise.vue'
+import question from '../views/index/question/question.vue'
+import subject from '../views/index/subject/subject.vue'
+import user from '../views/index/user/user.vue'
+
 // 路由实例化
 const router = new VueRouter({
   routes: [
@@ -14,7 +21,34 @@ const router = new VueRouter({
     },
     {
       path: '/index',
-      component: index
+      component: index,
+      // 嵌套的路由规则
+      children: [
+        {
+          // 地址不需要写/
+          // 最终解析的是 /index/chart'
+          path: 'chart',
+          component: chart
+        },
+        {
+          // 地址不需要写/
+          // 最终解析的是 /index/enterprise
+          path: 'enterprise',
+          component: enterprise
+        },
+        {
+          path: 'question',
+          component: question
+        },
+        {
+          path: 'subject',
+          component: subject
+        },
+        {
+          path: 'user',
+          component: user
+        },
+      ]
     },
   ]
 });
